b2a97b7deb855b9d305faef50328b346d489bcb0
alibha04 <alibha.pra@gmail.com>
2024-10-29 17:19:24 +0000

    express APIl2 practice

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 
-router.post('/books', bookController.createBook);
+// router.post('/books', bookController.createBook);
 router.get('/books', bookController.getAllBooks);
 router.get('/books/:id', bookController.getBookById);
 router.put('/books/:id', bookController.updateBook);
