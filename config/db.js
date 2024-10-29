7f084a72f036c93ad3d79acb725788a316fb33ca
alibha04 <alibha.pra@gmail.com>
2024-10-29 17:21:01 +0000

    express APIl2 practice

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    // try {
+    try {
         await mongoose.connect('mongodb://localhost:27017/mydatabase');
         console.log('Connected to MongoDB');
     } catch (err) {
